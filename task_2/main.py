162de6d8a5c5e6d591083ffc1bd5edabc79ba03b
MercyFlesh <con2288@mail.ru>
2020-12-04 23:43:14 +0300

    Added series of calculations

--- task_2/main.py
+++ task_2/main.py
@@ -26,37 +26,76 @@ def main():
     intervals = [(processor_mhz[0], processor_mhz[0] + h)]
     m = [0]
     s = [0]
-    p = [0]
-    q = [0]
-    
-    for _, row in df.iterrows():
-        if intervals[len(intervals)-1][1] >= row['Mhz']:
-            m[len(m)-1] += int(row['m'])
-            p[len(p)-1] += row['p']
+    p = list()
+    q = list()
+
+    for mhz, absolute in zip(processor_mhz, abs_frequencies):
+        if intervals[len(intervals)-1][1] >= mhz:
+            m[len(m)-1] += absolute
         else:
             intervals.append(
                 (intervals[len(intervals)-1][1], intervals[len(intervals)-1][1] + h)
             )
             s[len(s)-1] += m[len(m)-1]
-            q[len(q)-1] += p[len(p)-1]
+            p.append(round(m[len(m)-1] / df_size,4))
+            q.append(round(s[len(s)-1] / df_size,4))
+            
             s.append(s[len(s)-1])
-            q.append(q[len(q)-1])
-
-            m.append(int(row['m']))
-            p.append(row['p'])
+            m.append(absolute)
 
     s[len(s)-1] += m[len(m)-1]
-    q[len(q)-1] += p[len(p)-1]
-    
+    p.append(round(m[len(m)-1] / df_size,4))
+    q.append(round(s[len(s)-1] / df_size,4))
+    middles = list(map(lambda item: np.mean(item), intervals))
+
     interval_series_df = pd.DataFrame({
         'interval': intervals,
-        'middles': list(map(lambda item: np.mean(item), intervals)),
+        'middles': middles,
         'm': m, 'p': p,
         's': s, 'q': q
     })
         
-    print(interval_series_df)
+    print(interval_series_df, end='\n\n')
     
+    C = middles[math.ceil(len(middles)/2)-1]
+    z = list(map(lambda mid: int((mid - C) / h), middles))
+    
+    interval_series_df['con_vars'] = z
+    print(interval_series_df, end='\n\n')
+
+    M = list()
+    for i in range(1, 5):
+        M.append(sum(list(map(lambda m_i, z_i: (m_i * (z_i ** i)) / df_size, m, z))))
+
+    μ_2 = M[1] - (M[0] ** 2)
+    μ_3 = M[2] - (3 * M[1] * M[0]) + (2 * (M[0] ** 3))
+    μ_4 = M[3] - (4 * M[2] * M[0]) + (6 * M[1] * (M[0] ** 2)) - (3 * (M[0] ** 4))
+    μ = [0, μ_2, μ_3, μ_4]
+
+    moments_df = pd.DataFrame({
+        'M*': M,
+        'μ*': μ
+    })
+    moments_df.index = moments_df.index + 1
+    
+    print(moments_df, end='\n\n')
+
+    x_sample_mean = sum(list(map(lambda x, m: x * m, middles, m))) / df_size
+    print(f"Sample mean: {round(x_sample_mean, 5)}")
+    assert x_sample_mean == M[0] * h + C, "Error: the sample mean is not equal in two different formulas"
+
+    D = sum(list(map(lambda x_i, m_i: ((x_i - x_sample_mean) ** 2) * m_i, middles, m))) / df_size
+
+    print(f"Sample dispersion: {round(D, 5)}")   
+
+    assert D == round(μ_2 * (h ** 2), 10), "Error: the sample dispersion is not equal in two different formulas"
+
+
+
+    
+
+
+
     
 if __name__ == "__main__":
     main()
